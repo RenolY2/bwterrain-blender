c4599715f246310aba029c81d5fb64ad909e1efd
Yoshi2 <RenolY2@users.noreply.github.com>
2025-08-27 12:03:57 +0200

    Fix syntax errors preventing it from loading

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 bl_info = {
     "name": "Battalion Wars Terrain Addon",
-    "description": "Imports, modifies and exports Battalion Wars 1 terrain (.out)"
+    "description": "Imports, modifies and exports Battalion Wars 1 terrain (.out)",
     "author": "Yoshi2",
     "version": (1, 1, 0),
     "blender": (4, 5, 0),
     "category": "Import-Export",
-    "location": "File>Import/Export;View3D>Side Bar>BW Terrain tab"
+    "location": "File>Import/Export;View3D>Side Bar>BW Terrain tab",
     "warning": "Import/Export can take some time (>15 sec)"
 }
 from . import terrain_addon
